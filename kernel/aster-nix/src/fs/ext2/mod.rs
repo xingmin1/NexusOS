asterinas/asterinas

Support big file for Ext2

services/libs/aster-std/src/fs/ext2/mod.rs
@@ -33,19 +33,20 @@
 //! # Limitation
 //!
 //! Here we summarizes the features that need to be implemented in the future.
-//! 1. Supports large file.
-//! 2. Supports merging small read/write operations.
-//! 3. Handles the intermediate failure status correctly.
+//! 1. Supports merging small read/write operations.
+//! 2. Handles the intermediate failure status correctly.
 
 pub use fs::Ext2;
 pub use inode::{FilePerm, FileType, Inode};
 pub use super_block::{SuperBlock, MAGIC_NUM};
 
+mod bid_path;
 mod block_group;
 mod blocks_hole;
 mod dir;
 mod fs;
 mod impl_for_vfs;
+mod indirect_cache;
 mod inode;
 mod prelude;
 mod super_block;

xingmin1/NexusOS

kernel/aster-nix/src/fs/ext2/mod.rs
@@ -33,19 +33,20 @@
 //! # Limitation
 //!
 //! Here we summarizes the features that need to be implemented in the future.
-//! 1. Supports large file.
-//! 2. Supports merging small read/write operations.
-//! 3. Handles the intermediate failure status correctly.
+//! 1. Supports merging small read/write operations.
+//! 2. Handles the intermediate failure status correctly.
 
 pub use fs::Ext2;
 pub use inode::{FilePerm, FileType, Inode};
 pub use super_block::{SuperBlock, MAGIC_NUM};
 
 mod block_group;
+mod block_ptr;
 mod blocks_hole;
 mod dir;
 mod fs;
 mod impl_for_vfs;
+mod indirect_block_cache;
 mod inode;
 mod prelude;
 mod super_block;
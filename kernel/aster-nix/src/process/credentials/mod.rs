Plucky923/asterinas

Add syscall capget/capset

kernel/aster-nix/src/process/credentials/mod.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 
+pub mod c_types;
 mod credentials_;
 mod group;
+pub mod linuxcapability;
 mod static_cap;
 mod user;
 
@@ -20,7 +22,8 @@ use crate::prelude::*;
 /// - effective user ID and group ID;
 /// - saved-set user ID and saved-set group ID;
 /// - file system user ID and group ID (Linux-specific);
-/// - supplementary group IDs.
+/// - supplementary group IDs;
+/// - Linux capabilities.
 pub struct Credentials<R = FullOp>(Arc<Credentials_>, R);
 
 /// Gets read-only credentials of current thread.

kernel/aster-nix/src/process/credentials/static_cap.rs
@@ -4,7 +4,7 @@ use aster_frame::sync::{RwLockReadGuard, RwLockWriteGuard};
 use aster_rights::{Dup, Read, TRights, Write};
 use aster_rights_proc::require;
 
-use super::{credentials_::Credentials_, Credentials, Gid, Uid};
+use super::{credentials_::Credentials_, linuxcapability::Capability, Credentials, Gid, Uid};
 use crate::prelude::*;
 
 impl<R: TRights> Credentials<R> {
@@ -12,7 +12,8 @@ impl<R: TRights> Credentials<R> {
     pub fn new_root() -> Self {
         let uid = Uid::new_root();
         let gid = Gid::new_root();
-        let credentials_ = Arc::new(Credentials_::new(uid, gid));
+        let cap = Capability::new_root();
+        let credentials_ = Arc::new(Credentials_::new(uid, gid, cap));
         Self(credentials_, R::new())
     }
 
@@ -246,5 +247,55 @@ impl<R: TRights> Credentials<R> {
     #[require(R > Write)]
     pub fn groups_mut(&self) -> RwLockWriteGuard<BTreeSet<Gid>> {
         self.0.groups_mut()
+    }
+
+    // *********** Linux Capability methods **********
+
+    /// Gets the Linux caps that children can inherit.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn inheritablecap(&self) -> Capability {
+        self.0.inheritablecap()
+    }
+
+    /// Gets the Linux caps that caps are permitted.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn permittedcap(&self) -> Capability {
+        self.0.permittedcap()
+    }
+
+    /// Gets the Linux caps that actually use.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn effectivecap(&self) -> Capability {
+        self.0.effectivecap()
+    }
+
+    /// Sets the Linux caps that children can inherit.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_inheritablecap(&self, inheritablecap: Capability) {
+        self.0.set_inheritablecap(inheritablecap);
+    }
+
+    /// Sets the Linux caps that caps are permitted.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_permittedcap(&self, permittedcap: Capability) {
+        self.0.set_permittedcap(permittedcap);
+    }
+
+    /// Sets the Linux caps that actually use.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_effectivecap(&self, effectivecap: Capability) {
+        self.0.set_effectivecap(effectivecap);
     }
 }

kernel/aster-nix/src/syscall/capget.rs
@@ -1,0 +1,50 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use super::SyscallReturn;
+use crate::{
+    prelude::*,
+    process::{
+        credentials,
+        credentials::c_types::{cap_user_data_t, cap_user_header_t, LINUX_CAPABILITY_VERSION_3},
+    },
+    util::{read_val_from_user, write_val_to_user},
+};
+
+pub fn sys_capget(cap_user_header_addr: Vaddr, cap_user_data_addr: Vaddr) -> Result<SyscallReturn> {
+    let cap_user_header: cap_user_header_t =
+        read_val_from_user::<cap_user_header_t>(cap_user_header_addr)?;
+
+    // Validate the `header`.
+    if cap_user_header.version != LINUX_CAPABILITY_VERSION_3 {
+        // Return an error if the version is not supported.
+        // For simplicity, we assume that the header version is CAPABILITY_VERSION_3.
+        return_errno_with_message!(Errno::EINVAL, "not supported (capability version is not 3)");
+    };
+
+    // Extract target pid and validate whether it represents the current process.
+    let header_pid = cap_user_header.pid;
+    // Capget only query current process's credential. Namely, it only allows header->pid == 0
+    // or header->pid == getpid(), which are equivalent.
+    // See https://linux.die.net/man/2/capget (Section. With VFS capability support) for details.
+    if header_pid != 0 && header_pid != current!().pid() {
+        return_errno_with_message!(Errno::EINVAL, "invalid pid");
+    }
+
+    let credentials = credentials();
+    let inheritablecap = credentials.inheritablecap();
+    let permittedcap = credentials.permittedcap();
+    let effectivecap = credentials.effectivecap();
+
+    // Annoying legacy format with 64-bit capabilities exposed as two sets of 32-bit fields,
+    // so we need to split the capability values up.
+    let result = cap_user_data_t {
+        // Note we silently drop the upper capabilities here.
+        // This behavior is considered fail-safe behavior.
+        effective: effectivecap.as_u32(),
+        permitted: permittedcap.as_u32(),
+        inheritable: inheritablecap.as_u32(),
+    };
+
+    write_val_to_user(cap_user_data_addr, &result)?;
+    Ok(SyscallReturn::Return(0))
+}

xingmin1/NexusOS

kernel/aster-nix/src/process/credentials/mod.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
+pub mod c_types;
+pub mod capabilities;
 mod credentials_;
 mod group;
 mod static_cap;
@@ -20,7 +22,8 @@ use crate::prelude::*;
 /// - effective user ID and group ID;
 /// - saved-set user ID and saved-set group ID;
 /// - file system user ID and group ID (Linux-specific);
-/// - supplementary group IDs.
+/// - supplementary group IDs;
+/// - Linux capabilities.
 pub struct Credentials<R = FullOp>(Arc<Credentials_>, R);
 
 /// Gets read-only credentials of current thread.

kernel/aster-nix/src/process/credentials/static_cap.rs
@@ -6,7 +6,7 @@ use aster_frame::sync::{RwLockReadGuard, RwLockWriteGuard};
 use aster_rights::{Dup, Read, TRights, Write};
 use aster_rights_proc::require;
 
-use super::{credentials_::Credentials_, Credentials, Gid, Uid};
+use super::{capabilities::CapSet, credentials_::Credentials_, Credentials, Gid, Uid};
 use crate::prelude::*;
 
 impl<R: TRights> Credentials<R> {
@@ -14,7 +14,8 @@ impl<R: TRights> Credentials<R> {
     pub fn new_root() -> Self {
         let uid = Uid::new_root();
         let gid = Gid::new_root();
-        let credentials_ = Arc::new(Credentials_::new(uid, gid));
+        let cap = CapSet::new_root();
+        let credentials_ = Arc::new(Credentials_::new(uid, gid, cap));
         Self(credentials_, R::new())
     }
 
@@ -248,5 +249,55 @@ impl<R: TRights> Credentials<R> {
     #[require(R > Write)]
     pub fn groups_mut(&self) -> RwLockWriteGuard<BTreeSet<Gid>> {
         self.0.groups_mut()
+    }
+
+    // *********** Linux Capability methods **********
+
+    /// Gets the capabilities that child process can inherit.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn inheritable_capset(&self) -> CapSet {
+        self.0.inheritable_capset()
+    }
+
+    /// Gets the capabilities that are permitted.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn permitted_capset(&self) -> CapSet {
+        self.0.permitted_capset()
+    }
+
+    /// Gets the capabilities that actually use.
+    ///
+    /// This method requies the `Read` right.
+    #[require(R > Read)]
+    pub fn effective_capset(&self) -> CapSet {
+        self.0.effective_capset()
+    }
+
+    /// Sets the capabilities that child process can inherit.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_inheritable_capset(&self, inheritable_capset: CapSet) {
+        self.0.set_inheritable_capset(inheritable_capset);
+    }
+
+    /// Sets the capabilities that are permitted.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_permitted_capset(&self, permitted_capset: CapSet) {
+        self.0.set_permitted_capset(permitted_capset);
+    }
+
+    /// Sets the capabilities that actually use.
+    ///
+    /// This method requires the `Write` right.
+    #[require(R > Write)]
+    pub fn set_effective_capset(&self, effective_capset: CapSet) {
+        self.0.set_effective_capset(effective_capset);
     }
 }

kernel/aster-nix/src/syscall/capget.rs
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use super::SyscallReturn;
+use crate::{
+    prelude::*,
+    process::{
+        credentials,
+        credentials::c_types::{cap_user_data_t, cap_user_header_t, LINUX_CAPABILITY_VERSION_3},
+    },
+    util::{read_val_from_user, write_val_to_user},
+};
+
+pub fn sys_capget(cap_user_header_addr: Vaddr, cap_user_data_addr: Vaddr) -> Result<SyscallReturn> {
+    let cap_user_header: cap_user_header_t =
+        read_val_from_user::<cap_user_header_t>(cap_user_header_addr)?;
+
+    if cap_user_header.version != LINUX_CAPABILITY_VERSION_3 {
+        return_errno_with_message!(Errno::EINVAL, "not supported (capability version is not 3)");
+    };
+
+    // Extract target pid and validate whether it represents the current process.
+    let header_pid = cap_user_header.pid;
+    // Capget only query current process's credential. Namely, it only allows header->pid == 0
+    // or header->pid == getpid(), which are equivalent.
+    // See https://linux.die.net/man/2/capget (Section. With VFS capability support) for details.
+    if header_pid != 0 && header_pid != current!().pid() {
+        return_errno_with_message!(Errno::EINVAL, "invalid pid");
+    }
+
+    let credentials = credentials();
+    let inheritable_capset = credentials.inheritable_capset();
+    let permitted_capset = credentials.permitted_capset();
+    let effective_capset = credentials.effective_capset();
+
+    // Annoying legacy format with 64-bit capabilities exposed as two sets of 32-bit fields,
+    // so we need to split the capability values up.
+    let result = cap_user_data_t {
+        // Note we silently drop the upper capabilities here.
+        // This behavior is considered fail-safe behavior.
+        effective: effective_capset.as_u32(),
+        permitted: permitted_capset.as_u32(),
+        inheritable: inheritable_capset.as_u32(),
+    };
+
+    write_val_to_user(cap_user_data_addr, &result)?;
+    Ok(SyscallReturn::Return(0))
+}
asterinas/asterinas

Add sys_fallocate

kernel/aster-nix/src/fs/ext2/inode.rs
@@ -15,7 +15,7 @@ use super::{
     indirect_block_cache::{IndirectBlock, IndirectBlockCache},
     prelude::*,
 };
-use crate::time::clocks::RealTimeCoarseClock;
+use crate::{fs::utils::FallocateMode, time::clocks::RealTimeCoarseClock};
 
 /// Max length of file name.
 pub const MAX_FNAME_LEN: usize = 255;
@@ -622,6 +622,54 @@ impl Inode {
         inner.sync_data()?;
         inner.sync_metadata()?;
         Ok(())
+    }
+
+    pub fn fallocate(&self, mode: FallocateMode, offset: usize, len: usize) -> Result<()> {
+        if self.file_type() != FileType::File {
+            return_errno_with_message!(Errno::EISDIR, "not regular file");
+        }
+
+        match mode {
+            FallocateMode::PunchHoleKeepSize => {
+                // Make the whole operation atomic
+                let inner = self.inner.write();
+
+                let file_size = inner.file_size();
+                if offset >= file_size {
+                    return Ok(());
+                }
+                let end_offset = file_size.min(offset + len);
+
+                // TODO: Think of a more light-weight approach
+                inner.page_cache.zero_range(offset..end_offset)?;
+
+                // Mark the full blocks as holes
+                let inode_impl = inner.inode_impl.0.read();
+                let mut blocks_hole_desc = inode_impl.blocks_hole_desc.write();
+                for bid in Bid::from_offset(offset.align_up(BLOCK_SIZE))
+                    ..Bid::from_offset(end_offset.align_down(BLOCK_SIZE))
+                {
+                    blocks_hole_desc.set(bid.to_raw() as _);
+                }
+                Ok(())
+            }
+            // We extend the compatibility here since Ext2 in Linux
+            // does not natively support `Allocate` and `AllocateKeepSize`.
+            FallocateMode::Allocate => {
+                let new_size = offset + len;
+                if new_size > self.file_size() {
+                    self.resize(new_size)?;
+                }
+                Ok(())
+            }
+            FallocateMode::AllocateKeepSize => Ok(()),
+            _ => {
+                return_errno_with_message!(
+                    Errno::EOPNOTSUPP,
+                    "fallocate with the specified flags is not supported"
+                );
+            }
+        }
     }
 }
 

kernel/aster-nix/src/fs/file_handle.rs
@@ -8,7 +8,7 @@ use crate::{
     events::{IoEvents, Observer},
     fs::{
         device::Device,
-        utils::{AccessMode, InodeMode, IoctlCmd, Metadata, SeekFrom, StatusFlags},
+        utils::{AccessMode, FallocateFlags, InodeMode, IoctlCmd, Metadata, SeekFrom, StatusFlags},
     },
     net::socket::Socket,
     prelude::*,
@@ -104,6 +104,10 @@ pub trait FileLike: Send + Sync + Any {
 
     fn seek(&self, seek_from: SeekFrom) -> Result<usize> {
         return_errno_with_message!(Errno::ESPIPE, "seek is not supported");
+    }
+
+    fn fallocate(&self, flags: FallocateFlags, offset: usize, len: usize) -> Result<()> {
+        return_errno_with_message!(Errno::EOPNOTSUPP, "fallocate is not supported");
     }
 
     fn clean_for_close(&self) -> Result<()> {

kernel/aster-nix/src/fs/utils/inode.rs
@@ -7,7 +7,7 @@ use core::time::Duration;
 use aster_rights::Full;
 use core2::io::{Error as IoError, ErrorKind as IoErrorKind, Result as IoResult, Write};
 
-use super::{DirentVisitor, FileSystem, IoctlCmd};
+use super::{DirentVisitor, FallocateMode, FileSystem, IoctlCmd};
 use crate::{
     events::IoEvents,
     fs::device::{Device, DeviceType},
@@ -346,6 +346,12 @@ pub trait Inode: Any + Sync + Send {
 
     fn sync_data(&self) -> Result<()> {
         Ok(())
+    }
+
+    /// Manipulates a range of space of the file according to the specified allocate mode,
+    /// the manipulated range starts at `offset` and continues for `len` bytes.
+    fn fallocate(&self, mode: FallocateMode, offset: usize, len: usize) -> Result<()> {
+        return_errno!(Errno::EOPNOTSUPP);
     }
 
     fn poll(&self, mask: IoEvents, _poller: Option<&Poller>) -> IoEvents {

xingmin1/NexusOS

kernel/aster-nix/src/fs/ext2/inode.rs
@@ -16,6 +16,7 @@ use super::{
     prelude::*,
     utils::now,
 };
+use crate::fs::utils::FallocMode;
 
 /// Max length of file name.
 pub const MAX_FNAME_LEN: usize = 255;
@@ -733,6 +734,54 @@ impl Inode {
         let mut inner = self.inner.write();
         inner.set_gid(gid);
         inner.set_ctime(now());
+    }
+
+    pub fn fallocate(&self, mode: FallocMode, offset: usize, len: usize) -> Result<()> {
+        if self.file_type() != FileType::File {
+            return_errno_with_message!(Errno::EISDIR, "not regular file");
+        }
+
+        match mode {
+            FallocMode::PunchHoleKeepSize => {
+                // Make the whole operation atomic
+                let inner = self.inner.write();
+
+                let file_size = inner.file_size();
+                if offset >= file_size {
+                    return Ok(());
+                }
+                let end_offset = file_size.min(offset + len);
+
+                // TODO: Think of a more light-weight approach
+                inner.page_cache.fill_zeros(offset..end_offset)?;
+
+                // Mark the full blocks as holes
+                let inode_impl = inner.inode_impl.0.read();
+                let mut blocks_hole_desc = inode_impl.blocks_hole_desc.write();
+                for bid in Bid::from_offset(offset.align_up(BLOCK_SIZE))
+                    ..Bid::from_offset(end_offset.align_down(BLOCK_SIZE))
+                {
+                    blocks_hole_desc.set(bid.to_raw() as _);
+                }
+                Ok(())
+            }
+            // We extend the compatibility here since Ext2 in Linux
+            // does not natively support `Allocate` and `AllocateKeepSize`.
+            FallocMode::Allocate => {
+                let new_size = offset + len;
+                if new_size > self.file_size() {
+                    self.resize(new_size)?;
+                }
+                Ok(())
+            }
+            FallocMode::AllocateKeepSize => Ok(()),
+            _ => {
+                return_errno_with_message!(
+                    Errno::EOPNOTSUPP,
+                    "fallocate with the specified flags is not supported"
+                );
+            }
+        }
     }
 }
 

kernel/aster-nix/src/fs/file_handle.rs
@@ -8,7 +8,7 @@ use crate::{
     events::{IoEvents, Observer},
     fs::{
         device::Device,
-        utils::{AccessMode, InodeMode, IoctlCmd, Metadata, SeekFrom, StatusFlags},
+        utils::{AccessMode, FallocMode, InodeMode, IoctlCmd, Metadata, SeekFrom, StatusFlags},
     },
     net::socket::Socket,
     prelude::*,
@@ -96,6 +96,10 @@ pub trait FileLike: Pollable + Send + Sync + Any {
 
     fn seek(&self, seek_from: SeekFrom) -> Result<usize> {
         return_errno_with_message!(Errno::ESPIPE, "seek is not supported");
+    }
+
+    fn fallocate(&self, mode: FallocMode, offset: usize, len: usize) -> Result<()> {
+        return_errno_with_message!(Errno::EOPNOTSUPP, "fallocate is not supported");
     }
 
     fn register_observer(

kernel/aster-nix/src/fs/utils/inode.rs
@@ -7,7 +7,7 @@ use core::time::Duration;
 use aster_rights::Full;
 use core2::io::{Error as IoError, ErrorKind as IoErrorKind, Result as IoResult, Write};
 
-use super::{DirentVisitor, FileSystem, IoctlCmd};
+use super::{DirentVisitor, FallocMode, FileSystem, IoctlCmd};
 use crate::{
     events::IoEvents,
     fs::device::{Device, DeviceType},
@@ -346,6 +346,12 @@ pub trait Inode: Any + Sync + Send {
 
     fn sync_data(&self) -> Result<()> {
         Ok(())
+    }
+
+    /// Manipulates a range of space of the file according to the specified allocate mode,
+    /// the manipulated range starts at `offset` and continues for `len` bytes.
+    fn fallocate(&self, mode: FallocMode, offset: usize, len: usize) -> Result<()> {
+        return_errno!(Errno::EOPNOTSUPP);
     }
 
     fn poll(&self, mask: IoEvents, _poller: Option<&mut Poller>) -> IoEvents {
asterinas/asterinas

Enhance OSDK performance by using hard link instead of copy

osdk/src/bundle/bin.rs
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 use super::file::BundleFile;
-use crate::arch::Arch;
+use crate::{arch::Arch, util::fast_copy};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct AsterBin {
@@ -87,7 +84,7 @@ impl AsterBin {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fs::copy(&self.path, copied_path).unwrap();
+        fast_copy(&self.path, copied_path).unwrap();
         Self {
             path: PathBuf::from(file_name),
             arch: self.arch,

osdk/src/bundle/file.rs
@@ -7,6 +7,8 @@ use std::{
 
 use sha2::{Digest, Sha256};
 
+use crate::util::fast_copy;
+
 /// A trait for files in a bundle. The file in a bundle should have it's digest and be validatable.
 pub trait BundleFile {
     fn path(&self) -> &PathBuf;
@@ -57,7 +59,7 @@ impl Initramfs {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let name = self.path.file_name().unwrap();
         let dest = base.as_ref().join(name);
-        fs::copy(&self.path, dest).unwrap();
+        fast_copy(&self.path, dest).unwrap();
         Self {
             path: PathBuf::from(name),
             ..self

osdk/src/bundle/vm_image.rs
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
+
+use crate::util::fast_copy;
 
 use super::file::BundleFile;
 
@@ -63,8 +62,7 @@ impl AsterVmImage {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fs::copy(&self.path, copied_path).unwrap();
-        fs::remove_file(&self.path).unwrap();
+        fast_copy(&self.path, copied_path).unwrap();
         Self {
             path: PathBuf::from(file_name),
             typ: self.typ,

osdk/src/util.rs
@@ -3,7 +3,7 @@
 use std::{
     ffi::OsStr,
     fs::{self, File},
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Result, Write},
     path::{Path, PathBuf},
     process::Command,
 };
@@ -289,4 +289,25 @@ impl Drop for DirGuard {
     fn drop(&mut self) {
         std::env::set_current_dir(&self.0).unwrap();
     }
+}
+
+/// Attempts to create a hard link from `from` to `to`.
+/// If the hard link operation fails (e.g., due to crossing file systems),
+/// it falls back to performing a file copy.
+///
+/// # Arguments
+/// - `from`: The source file path.
+/// - `to`: The destination file path.
+///
+/// # Returns
+/// - `Ok(0)` if the hard link is successfully created (no data was copied).
+/// - `Ok(size)` where `size` is the number of bytes copied if the hard link failed and a copy was performed.
+/// - `Err(error)` if an error occurred during the copy operation.
+pub fn fast_copy<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> Result<u64> {
+    if fs::hard_link(&from, &to).is_err() {
+        info!("Copying {:?} -> {:?}", from.as_ref(), to.as_ref());
+        return fs::copy(from, to);
+    }
+    info!("Linking {:?} -> {:?}", from.as_ref(), to.as_ref());
+    Ok(0)
 }

xingmin1/NexusOS

osdk/src/bundle/bin.rs
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 use super::file::BundleFile;
-use crate::arch::Arch;
+use crate::{arch::Arch, util::hard_link_or_copy};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct AsterBin {
@@ -87,7 +84,7 @@ impl AsterBin {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fs::copy(&self.path, copied_path).unwrap();
+        hard_link_or_copy(&self.path, copied_path).unwrap();
         Self {
             path: PathBuf::from(file_name),
             arch: self.arch,

osdk/src/bundle/file.rs
@@ -7,6 +7,8 @@ use std::{
 
 use sha2::{Digest, Sha256};
 
+use crate::util::hard_link_or_copy;
+
 /// A trait for files in a bundle. The file in a bundle should have it's digest and be validatable.
 pub trait BundleFile {
     fn path(&self) -> &PathBuf;
@@ -57,7 +59,7 @@ impl Initramfs {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let name = self.path.file_name().unwrap();
         let dest = base.as_ref().join(name);
-        fs::copy(&self.path, dest).unwrap();
+        hard_link_or_copy(&self.path, dest).unwrap();
         Self {
             path: PathBuf::from(name),
             ..self

osdk/src/bundle/vm_image.rs
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
+
+use crate::util::hard_link_or_copy;
 
 use super::file::BundleFile;
 
@@ -63,8 +62,7 @@ impl AsterVmImage {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fs::copy(&self.path, copied_path).unwrap();
-        fs::remove_file(&self.path).unwrap();
+        hard_link_or_copy(&self.path, copied_path).unwrap();
         Self {
             path: PathBuf::from(file_name),
             typ: self.typ,

osdk/src/util.rs
@@ -3,7 +3,7 @@
 use std::{
     ffi::OsStr,
     fs::{self, File},
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Result, Write},
     path::{Path, PathBuf},
     process::Command,
 };
@@ -289,4 +289,25 @@ impl Drop for DirGuard {
     fn drop(&mut self) {
         std::env::set_current_dir(&self.0).unwrap();
     }
+}
+
+/// Attempts to create a hard link from `from` to `to`.
+/// If the hard link operation fails (e.g., due to crossing file systems),
+/// it falls back to performing a file copy.
+///
+/// # Arguments
+/// - `from`: The source file path.
+/// - `to`: The destination file path.
+///
+/// # Returns
+/// - `Ok(0)` if the hard link is successfully created (no data was copied).
+/// - `Ok(size)` where `size` is the number of bytes copied if the hard link failed and a copy was performed.
+/// - `Err(error)` if an error occurred during the copy operation.
+pub fn hard_link_or_copy<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> Result<u64> {
+    if fs::hard_link(&from, &to).is_err() {
+        info!("Copying {:?} -> {:?}", from.as_ref(), to.as_ref());
+        return fs::copy(from, to);
+    }
+    info!("Linking {:?} -> {:?}", from.as_ref(), to.as_ref());
+    Ok(0)
 }
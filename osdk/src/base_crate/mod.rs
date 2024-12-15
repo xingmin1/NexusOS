asterinas/asterinas

Enhance OSDK performance by reusing existing base, bundle and build

osdk/src/base_crate/mod.rs
@@ -5,11 +5,44 @@
 
 use std::{
     fs,
+    io::{Read, Result},
     path::{Path, PathBuf},
     str::FromStr,
 };
 
 use crate::util::get_cargo_metadata;
+
+/// Compares two files byte-by-byte to check if they are identical.
+/// Returns `Ok(true)` if files are identical, `Ok(false)` if they are different, or `Err` if any I/O operation fails.
+fn are_files_identical(file1: &PathBuf, file2: &PathBuf) -> Result<bool> {
+    // Check file size first
+    let metadata1 = fs::metadata(file1)?;
+    let metadata2 = fs::metadata(file2)?;
+
+    if metadata1.len() != metadata2.len() {
+        return Ok(false); // Different sizes, not identical
+    }
+
+    // Compare file contents byte-by-byte
+    let mut file1 = fs::File::open(file1)?;
+    let mut file2 = fs::File::open(file2)?;
+
+    let mut buffer1 = [0u8; 4096];
+    let mut buffer2 = [0u8; 4096];
+
+    loop {
+        let bytes_read1 = file1.read(&mut buffer1)?;
+        let bytes_read2 = file2.read(&mut buffer2)?;
+
+        if bytes_read1 != bytes_read2 || buffer1[..bytes_read1] != buffer2[..bytes_read1] {
+            return Ok(false); // Files are different
+        }
+
+        if bytes_read1 == 0 {
+            return Ok(true); // End of both files, identical
+        }
+    }
+}
 
 /// Create a new base crate that will be built by cargo.
 ///
@@ -17,6 +50,43 @@ use crate::util::get_cargo_metadata;
 /// `link_unit_test_runner` is set to true, the base crate will also depend on
 /// the `ostd-test-runner` crate.
 pub fn new_base_crate(
+    base_crate_path: impl AsRef<Path>,
+    dep_crate_name: &str,
+    dep_crate_path: impl AsRef<Path>,
+    link_unit_test_runner: bool,
+) {
+    // Check if the existing crate base is reusable. Crate bases for ktest are never reusable.
+    if !base_crate_path.as_ref().ends_with("base-test") && base_crate_path.as_ref().exists() {
+        let base_crate_tmp_path = base_crate_path.as_ref().join("tmp");
+        do_new_base_crate(
+            &base_crate_tmp_path,
+            dep_crate_name,
+            &dep_crate_path,
+            link_unit_test_runner,
+        );
+        let cargo_result = are_files_identical(
+            &base_crate_path.as_ref().join("Cargo.toml"),
+            &base_crate_tmp_path.join("Cargo.toml"),
+        );
+        let main_rs_result = are_files_identical(
+            &base_crate_path.as_ref().join("src").join("main.rs"),
+            &base_crate_tmp_path.join("src").join("main.rs"),
+        );
+        std::fs::remove_dir_all(&base_crate_tmp_path).unwrap();
+        if cargo_result.is_ok_and(|res| res) && main_rs_result.is_ok_and(|res| res) {
+            info!("Reusing existing base crate");
+            return;
+        }
+    }
+    do_new_base_crate(
+        base_crate_path,
+        dep_crate_name,
+        dep_crate_path,
+        link_unit_test_runner,
+    );
+}
+
+fn do_new_base_crate(
     base_crate_path: impl AsRef<Path>,
     dep_crate_name: &str,
     dep_crate_path: impl AsRef<Path>,

osdk/src/commands/new/mod.rs
@@ -47,9 +47,10 @@ fn add_manifest_dependencies(cargo_metadata: &serde_json::Value, crate_name: &st
     fs::write(manifest_path, content).unwrap();
 }
 
-// Add `target/osdk/base` to `exclude` array of the workspace manifest
+// Add `target/osdk/base` and `target/osdk/base-test` to `exclude` array of the workspace manifest
 fn exclude_osdk_base(metadata: &serde_json::Value) {
-    let osdk_base_path = "target/osdk/base";
+    let osdk_run_base_path = "target/osdk/base";
+    let osdk_test_base_path = "target/osdk/base-test";
 
     let workspace_manifest_path = {
         let workspace_root = metadata.get("workspace_root").unwrap().as_str().unwrap();
@@ -64,17 +65,25 @@ fn exclude_osdk_base(metadata: &serde_json::Value) {
 
         if let Some(exclude) = workspace.get_mut("exclude") {
             let exclude = exclude.as_array_mut().unwrap();
-            if exclude.contains(&toml::Value::String(osdk_base_path.to_string())) {
+            if exclude.contains(&toml::Value::String(osdk_run_base_path.to_string()))
+                || exclude.contains(&toml::Value::String(osdk_test_base_path.to_string()))
+            {
                 return;
             }
 
-            exclude.push(toml::Value::String(osdk_base_path.to_string()));
+            exclude.push(toml::Value::String(osdk_run_base_path.to_string()));
+            exclude.push(toml::Value::String(osdk_test_base_path.to_string()));
         } else {
-            let exclude = vec![toml::Value::String(osdk_base_path.to_string())];
+            let exclude = vec![
+                toml::Value::String(osdk_run_base_path.to_string()),
+                toml::Value::String(osdk_test_base_path.to_string()),
+            ];
             workspace.insert("exclude".to_string(), toml::Value::Array(exclude));
         }
     } else {
-        let exclude = toml::Table::from_str(r#"exclude = ["target/osdk/base"]"#).unwrap();
+        let exclude =
+            toml::Table::from_str(r#"exclude = ["target/osdk/base", "target/osdk/base-test"]"#)
+                .unwrap();
         manifest_toml.insert("workspace".to_string(), toml::Value::Table(exclude));
     }
 

osdk/src/commands/test.rs
@@ -26,7 +26,8 @@ pub fn test_current_crate(config: &Config, args: &TestArgs) {
     let current_crate = get_current_crate_info();
     let cargo_target_directory = get_target_directory();
     let osdk_output_directory = cargo_target_directory.join(DEFAULT_TARGET_RELPATH);
-    let target_crate_dir = osdk_output_directory.join("base");
+    // Use a different name for better separation and reusability of `run` and `test`
+    let target_crate_dir = osdk_output_directory.join("base-test");
 
     // A special case is that we use OSDK to test the OSDK test runner crate
     // itself. We check it by name.

osdk/tests/util/mod.rs
@@ -57,7 +57,10 @@ pub fn create_workspace(workspace_name: &str, members: &[&str]) {
             .map(|member| toml::Value::String(member.to_string()))
             .collect();
 
-        let exclude = toml::Value::Array(vec![toml::Value::String("target/osdk/base".to_string())]);
+        let exclude = toml::Value::Array(vec![
+            toml::Value::String("target/osdk/base".to_string()),
+            toml::Value::String("target/osdk/base-test".to_string()),
+        ]);
 
         table.insert("members".to_string(), toml::Value::Array(members));
         table.insert("exclude".to_string(), exclude);

xingmin1/NexusOS

osdk/src/base_crate/mod.rs
@@ -5,11 +5,44 @@
 
 use std::{
     fs,
+    io::{Read, Result},
     path::{Path, PathBuf},
     str::FromStr,
 };
 
 use crate::util::get_cargo_metadata;
+
+/// Compares two files byte-by-byte to check if they are identical.
+/// Returns `Ok(true)` if files are identical, `Ok(false)` if they are different, or `Err` if any I/O operation fails.
+fn are_files_identical(file1: &PathBuf, file2: &PathBuf) -> Result<bool> {
+    // Check file size first
+    let metadata1 = fs::metadata(file1)?;
+    let metadata2 = fs::metadata(file2)?;
+
+    if metadata1.len() != metadata2.len() {
+        return Ok(false); // Different sizes, not identical
+    }
+
+    // Compare file contents byte-by-byte
+    let mut file1 = fs::File::open(file1)?;
+    let mut file2 = fs::File::open(file2)?;
+
+    let mut buffer1 = [0u8; 4096];
+    let mut buffer2 = [0u8; 4096];
+
+    loop {
+        let bytes_read1 = file1.read(&mut buffer1)?;
+        let bytes_read2 = file2.read(&mut buffer2)?;
+
+        if bytes_read1 != bytes_read2 || buffer1[..bytes_read1] != buffer2[..bytes_read1] {
+            return Ok(false); // Files are different
+        }
+
+        if bytes_read1 == 0 {
+            return Ok(true); // End of both files, identical
+        }
+    }
+}
 
 /// Create a new base crate that will be built by cargo.
 ///
@@ -17,6 +50,43 @@ use crate::util::get_cargo_metadata;
 /// `link_unit_test_runner` is set to true, the base crate will also depend on
 /// the `ostd-test-runner` crate.
 pub fn new_base_crate(
+    base_crate_path: impl AsRef<Path>,
+    dep_crate_name: &str,
+    dep_crate_path: impl AsRef<Path>,
+    link_unit_test_runner: bool,
+) {
+    // Check if the existing crate base is reusable. Crate bases for ktest are never reusable.
+    if !base_crate_path.as_ref().ends_with("test-base") && base_crate_path.as_ref().exists() {
+        let base_crate_tmp_path = base_crate_path.as_ref().join("tmp");
+        do_new_base_crate(
+            &base_crate_tmp_path,
+            dep_crate_name,
+            &dep_crate_path,
+            link_unit_test_runner,
+        );
+        let cargo_result = are_files_identical(
+            &base_crate_path.as_ref().join("Cargo.toml"),
+            &base_crate_tmp_path.join("Cargo.toml"),
+        );
+        let main_rs_result = are_files_identical(
+            &base_crate_path.as_ref().join("src").join("main.rs"),
+            &base_crate_tmp_path.join("src").join("main.rs"),
+        );
+        std::fs::remove_dir_all(&base_crate_tmp_path).unwrap();
+        if cargo_result.is_ok_and(|res| res) && main_rs_result.is_ok_and(|res| res) {
+            info!("Reusing existing base crate");
+            return;
+        }
+    }
+    do_new_base_crate(
+        base_crate_path,
+        dep_crate_name,
+        dep_crate_path,
+        link_unit_test_runner,
+    );
+}
+
+fn do_new_base_crate(
     base_crate_path: impl AsRef<Path>,
     dep_crate_name: &str,
     dep_crate_path: impl AsRef<Path>,

osdk/src/commands/new/mod.rs
@@ -47,9 +47,10 @@ fn add_manifest_dependencies(cargo_metadata: &serde_json::Value, crate_name: &st
     fs::write(manifest_path, content).unwrap();
 }
 
-// Add `target/osdk/base` to `exclude` array of the workspace manifest
+// Add `target/osdk/base` and `target/osdk/test-base` to `exclude` array of the workspace manifest
 fn exclude_osdk_base(metadata: &serde_json::Value) {
-    let osdk_base_path = "target/osdk/base";
+    let osdk_run_base_path = "target/osdk/base";
+    let osdk_test_base_path = "target/osdk/test-base";
 
     let workspace_manifest_path = {
         let workspace_root = metadata.get("workspace_root").unwrap().as_str().unwrap();
@@ -64,17 +65,25 @@ fn exclude_osdk_base(metadata: &serde_json::Value) {
 
         if let Some(exclude) = workspace.get_mut("exclude") {
             let exclude = exclude.as_array_mut().unwrap();
-            if exclude.contains(&toml::Value::String(osdk_base_path.to_string())) {
+            if exclude.contains(&toml::Value::String(osdk_run_base_path.to_string()))
+                || exclude.contains(&toml::Value::String(osdk_test_base_path.to_string()))
+            {
                 return;
             }
 
-            exclude.push(toml::Value::String(osdk_base_path.to_string()));
+            exclude.push(toml::Value::String(osdk_run_base_path.to_string()));
+            exclude.push(toml::Value::String(osdk_test_base_path.to_string()));
         } else {
-            let exclude = vec![toml::Value::String(osdk_base_path.to_string())];
+            let exclude = vec![
+                toml::Value::String(osdk_run_base_path.to_string()),
+                toml::Value::String(osdk_test_base_path.to_string()),
+            ];
             workspace.insert("exclude".to_string(), toml::Value::Array(exclude));
         }
     } else {
-        let exclude = toml::Table::from_str(r#"exclude = ["target/osdk/base"]"#).unwrap();
+        let exclude =
+            toml::Table::from_str(r#"exclude = ["target/osdk/base", "target/osdk/test-base"]"#)
+                .unwrap();
         manifest_toml.insert("workspace".to_string(), toml::Value::Table(exclude));
     }
 

osdk/src/commands/test.rs
@@ -26,7 +26,8 @@ pub fn test_current_crate(config: &Config, args: &TestArgs) {
     let current_crate = get_current_crate_info();
     let cargo_target_directory = get_target_directory();
     let osdk_output_directory = cargo_target_directory.join(DEFAULT_TARGET_RELPATH);
-    let target_crate_dir = osdk_output_directory.join("base");
+    // Use a different name for better separation and reusability of `run` and `test`
+    let target_crate_dir = osdk_output_directory.join("test-base");
 
     // A special case is that we use OSDK to test the OSDK test runner crate
     // itself. We check it by name.

osdk/tests/util/mod.rs
@@ -57,7 +57,10 @@ pub fn create_workspace(workspace_name: &str, members: &[&str]) {
             .map(|member| toml::Value::String(member.to_string()))
             .collect();
 
-        let exclude = toml::Value::Array(vec![toml::Value::String("target/osdk/base".to_string())]);
+        let exclude = toml::Value::Array(vec![
+            toml::Value::String("target/osdk/base".to_string()),
+            toml::Value::String("target/osdk/test-base".to_string()),
+        ]);
 
         table.insert("members".to_string(), toml::Value::Array(members));
         table.insert("exclude".to_string(), exclude);
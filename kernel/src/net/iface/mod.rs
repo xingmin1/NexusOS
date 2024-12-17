asterinas/asterinas

Create backlog sockets on demand

kernel/src/net/iface/mod.rs
@@ -8,5 +8,8 @@ pub use init::{init, IFACES};
 pub use poll::lazy_init;
 
 pub type Iface = dyn aster_bigtcp::iface::Iface<ext::IfaceExt>;
-pub type BoundTcpSocket = aster_bigtcp::socket::BoundTcpSocket<ext::IfaceExt>;
-pub type BoundUdpSocket = aster_bigtcp::socket::BoundUdpSocket<ext::IfaceExt>;
+pub type BoundPort = aster_bigtcp::iface::BoundPort<ext::IfaceExt>;
+
+pub type TcpConnection = aster_bigtcp::socket::TcpConnection<ext::IfaceExt>;
+pub type TcpListener = aster_bigtcp::socket::TcpListener<ext::IfaceExt>;
+pub type UdpSocket = aster_bigtcp::socket::UdpSocket<ext::IfaceExt>;

xingmin1/NexusOS

kernel/src/net/iface/mod.rs
@@ -9,5 +9,8 @@ pub use init::{init, IFACES};
 pub use poll::lazy_init;
 
 pub type Iface = dyn aster_bigtcp::iface::Iface<ext::BigtcpExt>;
-pub type BoundTcpSocket = aster_bigtcp::socket::BoundTcpSocket<ext::BigtcpExt>;
-pub type BoundUdpSocket = aster_bigtcp::socket::BoundUdpSocket<ext::BigtcpExt>;
+pub type BoundPort = aster_bigtcp::iface::BoundPort<ext::BigtcpExt>;
+
+pub type TcpConnection = aster_bigtcp::socket::TcpConnection<ext::BigtcpExt>;
+pub type TcpListener = aster_bigtcp::socket::TcpListener<ext::BigtcpExt>;
+pub type UdpSocket = aster_bigtcp::socket::UdpSocket<ext::BigtcpExt>;
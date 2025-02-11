jellllly420/asterinas

Clean up the implementation of scheduling class

kernel/src/sched/sched_class/real_time.rs
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use alloc::collections::vec_deque::VecDeque;
+use alloc::{collections::VecDeque, sync::Arc};
 use core::{
     array,
     num::NonZero,
-    sync::atomic::{AtomicU8, Ordering::*},
+    sync::atomic::{AtomicU64, AtomicU8, Ordering::Relaxed},
 };
 
 use bitvec::{bitarr, BitArr};
+use ostd::{
+    cpu::CpuId,
+    task::{
+        scheduler::{EnqueueFlags, UpdateFlags},
+        Task,
+    },
+};
 
-use super::{time::base_slice_clocks, *};
+use super::{time::base_slice_clocks, CurrentRuntime, SchedAttr, SchedClassRq};
+use crate::{sched::priority::define_ranged_integer, thread::AsThread};
 
-pub type RtPrio = RangedU8<1, 99>;
+define_ranged_integer!(pub, RangedU8, u8);
+
+pub type RtPrio = RangedU8<0, 99>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum RealTimePolicy {

xingmin1/NexusOS

kernel/src/sched/sched_class/real_time.rs
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use alloc::collections::vec_deque::VecDeque;
+use alloc::{collections::VecDeque, sync::Arc};
 use core::{
     array,
     num::NonZero,
-    sync::atomic::{AtomicU8, Ordering::*},
+    sync::atomic::{AtomicU64, AtomicU8, Ordering::Relaxed},
 };
 
 use bitvec::{bitarr, BitArr};
+use ostd::{
+    cpu::CpuId,
+    task::{
+        scheduler::{EnqueueFlags, UpdateFlags},
+        Task,
+    },
+};
 
-use super::{time::base_slice_clocks, *};
+use super::{time::base_slice_clocks, CurrentRuntime, SchedAttr, SchedClassRq};
+use crate::{sched::priority::RangedU8, thread::AsThread};
 
-pub type RtPrio = RangedU8<1, 99>;
+pub type RtPrio = RangedU8<0, 99>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum RealTimePolicy {
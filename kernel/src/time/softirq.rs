asterinas/asterinas

Use `RcuOption` for lazy-initialized soft-IRQ callbacks

kernel/src/time/softirq.rs
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use alloc::{boxed::Box, vec::Vec};
+use alloc::{boxed::Box, vec, vec::Vec};
 
 use aster_softirq::{softirq_id::TIMER_SOFTIRQ_ID, SoftIrqLine};
-use ostd::{
-    sync::{LocalIrqDisabled, RwLock},
-    timer,
-};
+use ostd::{sync::RcuOption, timer};
 
-static TIMER_SOFTIRQ_CALLBACKS: RwLock<Vec<Box<dyn Fn() + Sync + Send>>, LocalIrqDisabled> =
-    RwLock::new(Vec::new());
+#[allow(clippy::type_complexity)]
+#[allow(clippy::box_collection)]
+static TIMER_SOFTIRQ_CALLBACKS: RcuOption<Box<Vec<fn()>>> = RcuOption::new_none();
 
 pub(super) fn init() {
     SoftIrqLine::get(TIMER_SOFTIRQ_ID).enable(timer_softirq_handler);
@@ -20,16 +18,41 @@ pub(super) fn init() {
 }
 
 /// Registers a function that will be executed during timer softirq.
-pub(super) fn register_callback<F>(func: F)
-where
-    F: Fn() + Sync + Send + 'static,
-{
-    TIMER_SOFTIRQ_CALLBACKS.write().push(Box::new(func));
+pub(super) fn register_callback(func: fn()) {
+    loop {
+        let callbacks = TIMER_SOFTIRQ_CALLBACKS.read();
+        match callbacks.try_get() {
+            // Initialized, copy the vector, push the function and update.
+            Ok(callbacks) => {
+                let mut callbacks_cloned = (*callbacks).clone();
+                callbacks_cloned.push(func);
+                if callbacks
+                    .compare_exchange(Box::new(callbacks_cloned))
+                    .is_ok()
+                {
+                    break;
+                }
+            }
+            // Uninitialized, initialize it.
+            Err(callbacks) => {
+                if callbacks
+                    .compare_exchange(Some(Box::new(vec![func])))
+                    .is_ok()
+                {
+                    break;
+                }
+            }
+        }
+        // Contention on initialization or pushing, retry.
+        core::hint::spin_loop();
+    }
 }
 
 fn timer_softirq_handler() {
     let callbacks = TIMER_SOFTIRQ_CALLBACKS.read();
-    for callback in callbacks.iter() {
-        (callback)();
+    if let Ok(callbacks) = callbacks.try_get() {
+        for callback in callbacks.iter() {
+            (callback)();
+        }
     }
 }

xingmin1/NexusOS

kernel/src/time/softirq.rs
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use alloc::{boxed::Box, vec::Vec};
+use alloc::{boxed::Box, vec, vec::Vec};
 
 use aster_softirq::{softirq_id::TIMER_SOFTIRQ_ID, SoftIrqLine};
-use ostd::{
-    sync::{LocalIrqDisabled, RwLock},
-    timer,
-};
+use ostd::{sync::RcuOption, timer};
 
-static TIMER_SOFTIRQ_CALLBACKS: RwLock<Vec<Box<dyn Fn() + Sync + Send>>, LocalIrqDisabled> =
-    RwLock::new(Vec::new());
+#[allow(clippy::type_complexity)]
+#[allow(clippy::box_collection)]
+static TIMER_SOFTIRQ_CALLBACKS: RcuOption<Box<Vec<fn()>>> = RcuOption::new_none();
 
 pub(super) fn init() {
     SoftIrqLine::get(TIMER_SOFTIRQ_ID).enable(timer_softirq_handler);
@@ -20,16 +18,41 @@ pub(super) fn init() {
 }
 
 /// Registers a function that will be executed during timer softirq.
-pub(super) fn register_callback<F>(func: F)
-where
-    F: Fn() + Sync + Send + 'static,
-{
-    TIMER_SOFTIRQ_CALLBACKS.write().push(Box::new(func));
+pub(super) fn register_callback(func: fn()) {
+    loop {
+        let callbacks = TIMER_SOFTIRQ_CALLBACKS.read();
+        match callbacks.get() {
+            // Initialized, copy the vector, push the function and update.
+            Some(callbacks_vec) => {
+                let mut callbacks_cloned = callbacks_vec.clone();
+                callbacks_cloned.push(func);
+                if callbacks
+                    .compare_exchange(Some(Box::new(callbacks_cloned)))
+                    .is_ok()
+                {
+                    break;
+                }
+            }
+            // Uninitialized, initialize it.
+            None => {
+                if callbacks
+                    .compare_exchange(Some(Box::new(vec![func])))
+                    .is_ok()
+                {
+                    break;
+                }
+            }
+        }
+        // Contention on initialization or pushing, retry.
+        core::hint::spin_loop();
+    }
 }
 
 fn timer_softirq_handler() {
     let callbacks = TIMER_SOFTIRQ_CALLBACKS.read();
-    for callback in callbacks.iter() {
-        (callback)();
+    if let Some(callbacks) = callbacks.get() {
+        for callback in callbacks.iter() {
+            (callback)();
+        }
     }
 }
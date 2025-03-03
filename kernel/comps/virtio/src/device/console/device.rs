asterinas/asterinas

Use RCU for console device callbacks

kernel/comps/virtio/src/device/console/device.rs
@@ -9,7 +9,7 @@ use log::debug;
 use ostd::{
     io_mem::IoMem,
     mm::{DmaDirection, DmaStream, DmaStreamSlice, FrameAllocOptions, VmReader},
-    sync::{RwLock, SpinLock},
+    sync::{Rcu, SpinLock},
     trap::TrapFrame,
 };
 
@@ -27,7 +27,8 @@ pub struct ConsoleDevice {
     transmit_queue: SpinLock<VirtQueue>,
     send_buffer: DmaStream,
     receive_buffer: DmaStream,
-    callbacks: RwLock<Vec<&'static ConsoleCallback>>,
+    #[allow(clippy::box_collection)]
+    callbacks: Rcu<Box<Vec<&'static ConsoleCallback>>>,
 }
 
 impl AnyConsoleDevice for ConsoleDevice {
@@ -54,7 +55,19 @@ impl AnyConsoleDevice for ConsoleDevice {
     }
 
     fn register_callback(&self, callback: &'static ConsoleCallback) {
-        self.callbacks.write_irq_disabled().push(callback);
+        loop {
+            let callbacks = self.callbacks.read();
+            let mut callbacks_cloned = callbacks.clone();
+            callbacks_cloned.push(callback);
+            if callbacks
+                .try_compare_update(Box::new(callbacks_cloned))
+                .is_ok()
+            {
+                break;
+            }
+            // Contention on pushing, retry.
+            core::hint::spin_loop();
+        }
     }
 }
 
@@ -103,7 +116,7 @@ impl ConsoleDevice {
             transmit_queue,
             send_buffer,
             receive_buffer,
-            callbacks: RwLock::new(Vec::new()),
+            callbacks: Rcu::new(Box::new(Vec::new())),
         });
 
         device.activate_receive_buffer(&mut device.receive_queue.disable_irq().lock());
@@ -136,7 +149,7 @@ impl ConsoleDevice {
         };
         self.receive_buffer.sync(0..len as usize).unwrap();
 
-        let callbacks = self.callbacks.read_irq_disabled();
+        let callbacks = self.callbacks.read();
         for callback in callbacks.iter() {
             let reader = self.receive_buffer.reader().unwrap().limit(len as usize);
             callback(reader);

xingmin1/NexusOS

kernel/comps/virtio/src/device/console/device.rs
@@ -7,7 +7,7 @@ use aster_console::{AnyConsoleDevice, ConsoleCallback};
 use log::debug;
 use ostd::{
     mm::{DmaDirection, DmaStream, DmaStreamSlice, FrameAllocOptions, VmReader},
-    sync::{LocalIrqDisabled, RwLock, SpinLock},
+    sync::{Rcu, SpinLock},
     trap::TrapFrame,
 };
 
@@ -25,7 +25,8 @@ pub struct ConsoleDevice {
     transmit_queue: SpinLock<VirtQueue>,
     send_buffer: DmaStream,
     receive_buffer: DmaStream,
-    callbacks: RwLock<Vec<&'static ConsoleCallback>, LocalIrqDisabled>,
+    #[expect(clippy::box_collection)]
+    callbacks: Rcu<Box<Vec<&'static ConsoleCallback>>>,
 }
 
 impl AnyConsoleDevice for ConsoleDevice {
@@ -52,7 +53,19 @@ impl AnyConsoleDevice for ConsoleDevice {
     }
 
     fn register_callback(&self, callback: &'static ConsoleCallback) {
-        self.callbacks.write().push(callback);
+        loop {
+            let callbacks = self.callbacks.read();
+            let mut callbacks_cloned = callbacks.clone();
+            callbacks_cloned.push(callback);
+            if callbacks
+                .compare_exchange(Box::new(callbacks_cloned))
+                .is_ok()
+            {
+                break;
+            }
+            // Contention on pushing, retry.
+            core::hint::spin_loop();
+        }
     }
 }
 
@@ -103,7 +116,7 @@ impl ConsoleDevice {
             transmit_queue,
             send_buffer,
             receive_buffer,
-            callbacks: RwLock::new(Vec::new()),
+            callbacks: Rcu::new(Box::new(Vec::new())),
         });
 
         device.activate_receive_buffer(&mut device.receive_queue.disable_irq().lock());
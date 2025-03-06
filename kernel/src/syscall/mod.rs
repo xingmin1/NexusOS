asterinas/asterinas

Add `sched_attr` system call series

kernel/src/syscall/mod.rs
@@ -95,6 +95,7 @@ mod rt_sigprocmask;
 mod rt_sigreturn;
 mod rt_sigsuspend;
 mod sched_affinity;
+mod sched_attr;
 mod sched_yield;
 mod select;
 mod semctl;

xingmin1/NexusOS

kernel/src/syscall/mod.rs
@@ -37,6 +37,7 @@ mod flock;
 mod fork;
 mod fsync;
 mod futex;
+mod get_priority;
 mod getcpu;
 mod getcwd;
 mod getdents64;
@@ -96,6 +97,14 @@ mod rt_sigprocmask;
 mod rt_sigreturn;
 mod rt_sigsuspend;
 mod sched_affinity;
+mod sched_get_priority_max;
+mod sched_get_priority_min;
+mod sched_getattr;
+mod sched_getparam;
+mod sched_getscheduler;
+mod sched_setattr;
+mod sched_setparam;
+mod sched_setscheduler;
 mod sched_yield;
 mod select;
 mod semctl;
@@ -104,7 +113,7 @@ mod semop;
 mod sendfile;
 mod sendmsg;
 mod sendto;
-mod set_get_priority;
+mod set_priority;
 mod set_robust_list;
 mod set_tid_address;
 mod setfsgid;
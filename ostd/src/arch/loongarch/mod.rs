2cbfcdd89fe6cf653b7f98790045abcaa0652a16
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-19 19:20:49 +0800

    feat(ostd): add LoongArch64 module

---
+++ ostd/src/arch/loongarch/mod.rs
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//! Platform-specific code for the LoongArch platform.
--- ostd/src/arch/mod.rs
+++ ostd/src/arch/mod.rs
@@ -4,11 +4,15 @@
 //!
 //! Each architecture that Asterinas supports may contain a submodule here.
 
+#[cfg(target_arch = "loongarch64")]
+pub mod loongarch;
 #[cfg(target_arch = "riscv64")]
 pub mod riscv;
 #[cfg(target_arch = "x86_64")]
 pub mod x86;
 
+#[cfg(target_arch = "loongarch64")]
+pub use self::loongarch::*;
 #[cfg(target_arch = "riscv64")]
 pub use self::riscv::*;
 #[cfg(target_arch = "x86_64")]

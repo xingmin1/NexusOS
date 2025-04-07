36666349f13e6437ff311f1930b0b550907ab0bf
Caiyi Shyu <cai1hsu@outlook.com>
2025-04-07 16:23:43 +0800

    fix(ostd/la): add late_init_on_bsp

--- ostd/src/arch/loongarch/mod.rs
+++ ostd/src/arch/loongarch/mod.rs
@@ -44,28 +44,18 @@ pub(crate) fn enable_cpu_features() {
     loongArch64::register::euen::set_fpe(true);
 }
 
-pub(crate) fn init_on_bsp() {
+pub(crate) unsafe fn late_init_on_bsp() {
     // SAFETY: this function is only called once on BSP.
     unsafe {
         trap::init(true);
     }
     irq::init();
 
-    // SAFETY: they are only called once on BSP and ACPI has been initialized.
-    unsafe {
-        crate::cpu::init_num_cpus();
-        crate::cpu::set_this_cpu_id(0);
-    }
-
-    // SAFETY: no CPU local objects have been accessed by this far. And
-    // we are on the BSP.
-    unsafe { crate::cpu::local::init_on_bsp() };
-
-    crate::sync::init();
-
-    crate::boot::smp::boot_all_aps();
+    // SAFETY: we're on the BSP and we're ready to boot all APs.
+    unsafe { crate::boot::smp::boot_all_aps() };
 
     timer::init();
+    let _ = crate::bus::pci::init();
 }
 
 pub(crate) unsafe fn init_on_ap() {
